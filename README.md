9cbd8c51cb023ef89a976bcc355d59ea03420278
JCS PRO <JCS-PRO@iMac-de-JCS.local>
2015-01-14 17:06:36 +0100

    installation

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 # js-slideshow
 Slideshow
+
+#CSS
+
+    // Generic slideshow
+    //==================
+    .mod-slideshow-01 {
+        @include absoluteLeft(0,0);
+        height:                 100%;
+        overflow:               hidden;
+        width:                  100%;
+        z-index:                0;
+        .collection {
+            @include absoluteLeft(0,0);
+            height:                 100%;
+            width:                  100%;
+            z-index:                0;
+            .slide {
+                @include absoluteLeft(0,0);
+                height:                 100%;
+                width:                  100%;
+                &.disabled {
+                    visibility:             hidden;
+                }
+                &.enabled {
+                    visibility:             visible;
+                }
+            }
+        }
+    }
+
+#JS
+
+    // Slideshow of the home
+    //=======================
+    Transition.prototype.slideshowGeneric = function(_scope) {
+        var
+            scope               =   _scope
+        ,   $ref                =   scope.$slideshow
+        ,   $panels             =   scope.panels
+        ,   current             =   parseInt($ref.data('index'))
+        ,   next                =   scope.currentIndex
+        ,   context             =   _k.getViewport()
+        ,   moveX               =   (scope.full)?context.width:scope.$slideshow.width()
+        ,   $next               =   $ref.find('.slide').eq(next)
+        ,   $current            =   $ref.find('.slide').eq(current)
+        ,   $nextPanel          =   $panels.eq(next)
+        ,   $currentPanel       =   $panels.eq(current)
+        ,   timeline            =   new TimelineLite({
+                paused : true
+            })
+        ,   duration            =   1.2
+        ;
+
+
+        if(current!=next)
+        {
+            scope.locked            =   true;
+            // timeline
+            //=========
+            timeline.kill();
+
+            var
+                endCurrent      =   (current>next)?moveX:(moveX*-1)
+            ,   startNext       =   (current>next)?(moveX*-1):moveX
+            ;
+
+            timeline.call(
+                function() {
+                    $current.css({
+                        'z-index' : 2
+                    });
+                    $next
+                        .css({
+                            'z-index' : 3
+                        })
+                        .removeClass('disabled')
+                        .addClass('enabled')
+                    ;
+                    $nextPanel
+                        .removeClass('disabled')
+                        .addClass('enabled')
+                    ;
+                    scope.updateInfo();
+                }
+            );
+            timeline.add(
+                [
+                    TweenLite.fromTo($current,duration ,{ x : 0 },{ x : endCurrent/1.8 , ease : Expo.easeInOut, force3D:true }),
+                    TweenLite.fromTo($currentPanel,duration ,{ clip : 'rect(0px, 310px, 120px, 0px)' },{ clip : 'rect(0px, 310px, 120px, 310px)' , ease : Expo.easeInOut, force3D:true }),
+                    TweenLite.fromTo($next,duration,{ x : startNext },{ x : 0 , ease : Expo.easeInOut, force3D:true }),
+                    TweenLite.fromTo($nextPanel,duration,{ clip : 'rect(0px, 0px, 120px, 0px)' },{ clip : 'rect(0px, 310px, 120px, 0px)' , ease : Expo.easeInOut, force3D:true })
+                ]
+            );
+
+
+            timeline.call(
+                function() {
+                    scope.locked    =   false;
+                    TweenLite.set($current,{clearProps:"all" });
+                    TweenLite.set($next,{clearProps:"all" });
+                    scope.store();
+                    scope.update();
+                }
+            );
+
+            timeline.restart();
+        }
+
+    }
---
+++ src/slideshow.js
@@ -0,0 +1,165 @@
+(function(window, document, undefined) {
+    'use strict';
+
+    SITE.slideshow = (function()
+    {
+
+        // Slideshow
+        //==========
+        var Generic = function(_view,_options) {
+            this.view           =   (typeof _view != 'undefined')?_view.find('#slideshowInstance'):$(document.getElementById('slideshowInstance'));
+            this.settings       =   (typeof _options != 'undefined')?_options:{};
+            this.$slideshow     =   this.view.find('.jsSlideshow--collection');
+            this.currentIndex   =   (typeof _options.start != 'undefined')?_options.start:0;
+            this.full           =   true;
+            this.locked         =   false;
+            this.items          =   this.$slideshow.find('.jsSlideshow--item');
+            this.triggers       =   this.view.find('.jsSlideshow--buttons');
+            this.total          =   this.items.length - 1;
+            this.interval       =   5000;
+            this.timerAutoplay  =   false;
+            this.init();
+        };
+        Generic.prototype = {
+            events : function() {
+                var
+                    __that       =   this
+                ;
+
+                if(__that.settings.autoplay == true)
+                {
+                    __that.timerAutoplay = window.setInterval(function() {
+                        __that.currentIndex++;
+                        if(__that.currentIndex > __that.total)
+                        {
+                            __that.currentIndex = 0;
+                        }
+                        __that.goTo(__that.currentIndex,1);
+                    },__that.interval);
+                }
+                __that.triggers.on('click',function() {
+                    if(!__that.locked)
+                    {
+                        var
+                            dir
+                        ;
+                        if($(this).index() > __that.currentIndex)
+                        {
+                            dir     =   -1;
+                        }
+                        else
+                        {
+                            dir     =   1;
+                        }
+                        __that.currentIndex   =   $(this).index();
+                        API_transition.slideshowGeneric(__that);
+                        clearInterval(__that.timerAutoplay);
+                    }
+
+                });
+                if(isTablet)
+                {
+                    __that.swipeOn();
+                }
+            }
+            ,
+            goTo : function(_index,_dir)
+            {
+                if(!this.locked)
+                {
+                    this.currentIndex   =   _index;
+                    API_transition.slideshowGeneric(this);
+                }
+            }
+            ,
+            goToPrevious : function() {
+                if(!this.locked)
+                {
+                    this.currentIndex--;
+                    if(this.currentIndex < 0)
+                    {
+                        this.currentIndex   =   0;
+                    }
+                    API_transition.slideshowGeneric(this);
+                }
+            }
+            ,
+            goToNext : function() {
+                if(!this.locked)
+                {
+                    this.currentIndex++;
+                    if(this.currentIndex > this.total)
+                    {
+                        this.currentIndex   =   this.total;
+                    }
+                    API_transition.slideshowGeneric(this);
+                }
+            }
+            ,
+            setup : function() {
+                this.store();
+                if(this.total == 0)
+                {
+                    this.triggers.addClass('vs-0');
+                }
+            }
+            ,
+            store : function() {
+                this.$slideshow.data('index',this.currentIndex);
+            }
+            ,
+            swipeOn : function() {
+                var
+                    refID       =   document.getElementById('main-content')
+                ,   __that   =   this
+                ;
+                Hammer(refID).on("swipeup", function(event) {
+                    __that.$next.trigger('click');
+                });
+                Hammer(refID).on("swipedown", function(event) {
+                    __that.$previous.trigger('click');
+                });
+            }
+            ,
+            update : function() {
+                this.items.removeClass('disabled enabled');
+                this.items.css({
+                    'z-index' : 0
+                }).addClass('disabled');
+                this.items.eq(this.currentIndex).css({
+                    'z-index':1
+                }).removeClass('disabled').addClass('enabled');
+                this.items.each(function() {
+                    if($(this).hasClass('enabled'))
+                    {
+                        $(this).addClass('dp-b')
+                    }
+                    else
+                    {
+                        $(this).addClass('dp-no')
+                    }
+                })
+            }
+            ,
+            updateInfo : function()
+            {
+
+                this.triggers.removeClass('active');
+                this.triggers.eq(this.currentIndex).addClass('active');
+                if(typeof this.settings.onUpdate != 'undefined' && typeof this.settings.onUpdate == 'function') this.settings.onUpdate(this.currentIndex,this.items.eq(this.currentIndex));
+            }
+            ,
+            init : function() {
+                this.setup();
+                this.events();
+                this.update();
+                this.updateInfo();
+            }
+        }
+
+        return {
+                Generic: Generic
+        }
+    })();
+
+}(window, document));
